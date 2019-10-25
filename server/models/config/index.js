fa438f87576665e2a81a720ba7b4f7ed4c1d95e6
MaiUbeid <ubead95@gmail.com>
2019-10-26 00:20:03 +0300

    replace Components with components #20

--- server/models/config/index.js
+++ server/models/config/index.js
@@ -1,8 +1,6 @@
 const Airtable = require('airtable');
 require('dotenv').config();
 
-Airtable.configure({
-  endpointUrl: 'https://api.airtable.com',
-  apiKey: process.env.AIRTABLE_API_KEY,
-});
-// const base = Airtable.base(process.env.AIRTABLE_BASE_ID);
+const base = Airtable.base(process.env.AIRTABLE_BASE_ID);
+
+module.exports = base;
