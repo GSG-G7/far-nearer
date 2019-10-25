6232a7bb6af57360163a28dbf05f962a6fc03032
MaiUbeid <ubead95@gmail.com>
2019-10-25 15:53:05 +0300

    resolve disable powered by and set port #20

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
-const { join } = require('path');
 const express = require('express');
+const { join } = require('path');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const morgan = require('morgan');
@@ -8,15 +8,17 @@ const controllers = require('./controllers');
 
 const app = express();
 
+app.disable('x-powered-by');
+app.set('port', process.env.PORT || 5000);
+
+app.use(compression());
 app.use(express.json());
 app.use(cookieParser());
-app.set('port', process.env.PORT || 5000);
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(compression());
 app.use('/api/v1/', controllers);
 
 app.use(express.static(join(__dirname, '..', 'client', 'build')));
