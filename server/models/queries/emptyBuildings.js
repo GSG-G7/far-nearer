cee4c4df831e65a18d50bafbf3b8e9800ed18bbe
rana <ranasobeid95@gmail.com>
2019-10-27 21:24:37 +0200

    Create a query to insert new building to empty_buildings table

---
+++ server/models/queries/emptyBuildings.js
@@ -0,0 +1,42 @@
+const base = require('../config');
+
+const reportBuilding = ({
+  city,
+  latitude,
+  longitude,
+  address,
+  previousUse,
+  preferredUse,
+  owner,
+  isOwnerLocal,
+  emptyPeriod,
+  extraInfo,
+  approved,
+  receiveNotifications,
+  reporterName,
+  reporterEmail,
+  reporterAddress,
+}) => {
+  return base('empty_buildings').create([
+    {
+      fields: {
+        city,
+        latitude,
+        longitude,
+        address,
+        owner,
+        preferred_use: preferredUse,
+        previous_use: previousUse,
+        is_owner_local: isOwnerLocal,
+        empty_period: emptyPeriod,
+        extra_info: extraInfo,
+        approved,
+        recieve_notifications: receiveNotifications,
+        reporter_name: reporterName,
+        reporter_email: reporterEmail,
+        reporter_address: reporterAddress,
+      },
+    },
+  ]);
+};
+module.exports = reportBuilding;
