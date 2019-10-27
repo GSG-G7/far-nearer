c13a285a97dc3026bd88525709c19d2de142465b
rana <ranasobeid95@gmail.com>
2019-10-27 21:43:53 +0200

    exports postEmptyBuilding

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+const { postEmptyBuilding } = require('./emptyBuildings');
+
 router.get('/', (req, res) => {
   res.send({ data: 'Hello from server' });
 });
+
+router.post('/report-building', postEmptyBuilding);
 
 module.exports = router;
