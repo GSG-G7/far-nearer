825a52d55e0bdf6306af726e12abc8ae6d346b56
MaiUbeid <ubead95@gmail.com>
2019-10-28 09:41:57 +0200

    fix test for validation and already exist #27

--- test/mailList.test.js
+++ test/mailList.test.js
@@ -5,12 +5,12 @@ const app = require('../server/app');
 
 tape('test mail list route with validation error', t => {
   supertest(app)
-    .get('/api/v1/mailList')
+    .get('/api/v1/mailList?email=mgmail.com')
     .expect(400)
     .expect('Content-Type', 'application/json; charset=utf-8')
     .end((err, res) => {
       if (err) {
-        t.error();
+        t.error(err);
         t.end();
       } else {
         t.equals(
@@ -25,16 +25,16 @@ tape('test mail list route with validation error', t => {
 
 tape('test mail list route with already exist email', t => {
   supertest(app)
-    .get('/api/v1/mailList')
-    .expect(201)
+    .get('/api/v1/mailList?email=m@gmail.com')
+    .expect(200)
     .expect('Content-Type', 'application/json; charset=utf-8')
     .end((err, res) => {
       if (err) {
-        t.error();
+        t.error(err);
         t.end();
       } else {
         t.equals(
-          res.text.includes('Email is exist'),
+          res.text.includes('Email already exists'),
           true,
           'Should have Email is exist',
         );
