4b1d9857c3ac23dce1375d4ed5fac87177070714
FadiAlamassi <alamassifadi@gmail.com>
2019-10-28 09:37:18 +0200

    Modify the response and send the status code in it Relates #25

--- server/controllers/emptyBuildings/get.js
+++ server/controllers/emptyBuildings/get.js
@@ -3,7 +3,7 @@ const { get } = require('../../models/queries/emptyBuildings');
 module.exports = async (req, res, next) => {
   try {
     const buildings = await get();
-    res.json(buildings);
+    res.json({ data: buildings, statusCode: 200 });
   } catch (err) {
     next(err);
   }
