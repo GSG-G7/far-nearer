299d5c1cb6864907b732899be868189bc2bf8578
FadiAlamassi <alamassifadi@gmail.com>
2019-10-28 09:21:05 +0200

    Create route to handle the empty-buildings endpoint Relates #25

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+const { getEmptyBuildings } = require('./emptyBuildings');
+
 router.get('/', (req, res) => {
   res.send({ data: 'Hello from server' });
 });
+
+router.get('/empty-buildings', getEmptyBuildings);
 
 module.exports = router;
