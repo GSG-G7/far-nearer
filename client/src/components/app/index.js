5b275cd44434cbf85410c9978f830695043f95ca
MaiUbeid <ubead95@gmail.com>
2019-10-31 11:27:51 +0200

    remove flex-direction #36

--- client/src/components/app/index.js
+++ client/src/components/app/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 
+import Footer from '../utils/Footer';
+
 import './style.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello From React</h1>
+      <Footer />
     </div>
   );
 }
