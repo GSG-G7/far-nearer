e2a33d81b94fef553d6b8da63ba5a4ab753435a0
MaiUbeid <ubead95@gmail.com>
2019-10-31 14:17:00 +0200

    add duration for the notification #36

--- client/src/components/utils/Footer/Subscribe.js
+++ client/src/components/utils/Footer/Subscribe.js
@@ -14,6 +14,7 @@ class SubscribeForm extends Component {
     const openNotificationWithIcon = (type, message) => {
       notification[type]({
         message,
+        duration: 3,
       });
     };
     validateFields(async (err, values) => {
