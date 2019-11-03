5acdc261153e573ad59066497f2cd2041d40875f
Mai Ubeid <ubead95@gmail.com>
2019-11-03 15:01:10 +0200

    Update client/src/components/app/index.js
    Co-Authored-By: Ahmed I. Abdellatif <ahmedisam9922@gmail.com>

--- client/src/components/app/index.js
+++ client/src/components/app/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'antd/dist/antd.css';
 import './style.css';
 
-import { Footer } from '../utils';
+import { Footer } from 'components/utils';
 import About from '../pages/AboutUs';
 
 function App() {
