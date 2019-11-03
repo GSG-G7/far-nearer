79faef9ca42298cc45cb8e467dd2628603ec3771
MaiUbeid <ubead95@gmail.com>
2019-11-03 14:47:32 +0200

    change antd dist #66

--- client/src/components/app/index.js
+++ client/src/components/app/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import 'antd/dist/antd.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import 'antd/dist/antd.css';
 import './style.css';
-import { Navbar } from 'components/utils';
 
 function App() {
   return (
     <div className="App">
-      {/* <h1>Hello From React</h1> */}
-      <Router>
-        <Switch>
-          <Route>
-            <Navbar />
-          </Route>
-        </Switch>
-      </Router>
+      <h1>Hello From React</h1>
     </div>
   );
 }
