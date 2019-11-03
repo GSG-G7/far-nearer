b141e9f2034d96fc8beb2fe4d362f76d69ebefba
MaiUbeid <ubead95@gmail.com>
2019-11-03 14:33:20 +0200

    remove props from about route #35

--- client/src/components/app/index.js
+++ client/src/components/app/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import 'antd/dist/antd.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import About from '../pages/AboutUs';
-
+import 'antd/dist/antd.css';
 import './style.css';
+
+import About from '../pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/about" render={props => <About />} />
+          <Route exact path="/about" component={About} />
         </Switch>
       </Router>
     </div>
