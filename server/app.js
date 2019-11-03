c8167b945219dc08dac0f1bdc333a92270ba782f
rana <ranasobeid95@gmail.com>
2019-11-03 09:14:11 +0200

    use helmet  as a middleware Relates #56

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const morgan = require('morgan');
 const fileupload = require('express-fileupload');
+const helmet = require('helmet');
 
 const controllers = require('./controllers');
 const errorHandle = require('./controllers/middleware');
@@ -13,6 +14,7 @@ const app = express();
 app.disable('x-powered-by');
 app.set('port', process.env.PORT || 5000);
 
+app.use(helmet());
 app.use(express.json());
 app.use(compression());
 app.use(cookieParser());
