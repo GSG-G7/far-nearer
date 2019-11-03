9863e4a383625a00593856592f708a3b90f32024
MaiUbeid <ubead95@gmail.com>
2019-11-03 15:09:36 +0200

    add underConstruction in index for utils #66

--- client/src/components/utils/index.js
+++ client/src/components/utils/index.js
@@ -2,5 +2,6 @@
 import Loading from './Loading';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import UnderConstruction from './UnderConstruction';
 
-export { Loading, Footer, Navbar };
+export { Loading, Footer, Navbar, UnderConstruction };
