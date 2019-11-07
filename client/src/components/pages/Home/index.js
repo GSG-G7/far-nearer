3eb5dc2f35e4bcca2e4d291397ac9fef803c45de
FadiAlamassi <alamassifadi@gmail.com>
2019-11-07 10:01:10 +0200

    Create home page Relates #31

---
+++ client/src/components/pages/Home/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { message } from 'antd';
+
+import Header from './Header';
+import About from './About';
+import ReportBuilding from './ReportBuilding';
+import { Navbar, Footer } from '../../utils';
+
+class Home extends Component {
+  state = {
+    city: 'Morecambe',
+  };
+
+  handleCityChange = ({ key }) => {
+    const city = +key === 1 ? 'Morecambe' : 'Hastings';
+    this.setState({ city });
+    message.success(`${city} selected `);
+  };
+
+  render() {
+    const { city } = this.state;
+    return (
+      <>
+        <Navbar transparent />
+        <Header onCityChange={this.handleCityChange} />
+        <About />
+        <ReportBuilding city={city} onCityChange={this.handleCityChange} />
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default Home;
