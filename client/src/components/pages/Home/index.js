42582a8eeb03c2e78f8cf269bf07aa014cbdbb1a
FadiAlamassi <alamassifadi@gmail.com>
2019-11-11 09:36:33 +0200

    Create propTypes, handle the changes and pass props for the report building component Relates #31

--- client/src/components/pages/Home/index.js
+++ client/src/components/pages/Home/index.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import { message } from 'antd';
+import PropTypes from 'prop-types';
 
 import Header from './Header';
 import About from './About';
 import ReportBuilding from './ReportBuilding';
-import { Navbar, Footer } from '../../utils';
+import { Navbar } from '../../utils';
 
 class Home extends Component {
   state = {
     city: 'Morecambe',
+    address: '',
+    longitude: '',
+    latitude: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const {
+      form: { validateFields },
+    } = this.props;
+    validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  handleAddressChange = (markerCoordinates, address) => {
+    if (markerCoordinates && address) {
+      const { lat: latitude, lng: longitude } = markerCoordinates;
+      this.setState({ latitude, longitude, address });
+    }
   };
 
   handleCityChange = ({ key }) => {
@@ -18,16 +41,26 @@ class Home extends Component {
   };
 
   render() {
-    const { city } = this.state;
+    const { city, latitude, longitude, address } = this.state;
     return (
       <>
         <Navbar transparent />
         <Header onCityChange={this.handleCityChange} />
         <About />
-        <ReportBuilding city={city} onCityChange={this.handleCityChange} />
+        <ReportBuilding
+          city={city}
+          address={address}
+          markerCoordinates={{ lat: latitude, lng: longitude }}
+          onCityChange={this.handleCityChange}
+          handleAddressChange={this.handleAddressChange}
+        />
       </>
     );
   }
 }
+
+Home.propTypes = {
+  form: PropTypes.objectOf(PropTypes.func).isRequired,
+};
 
 export default Home;
