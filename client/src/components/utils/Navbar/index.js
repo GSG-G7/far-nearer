e831f4ca8bf6a2be0248f01cc7c959f80dd3a3d5
MaiUbeid <ubead95@gmail.com>
2019-11-19 22:48:08 +0200

    overlay the screen #92

--- client/src/components/utils/Navbar/index.js
+++ client/src/components/utils/Navbar/index.js
@@ -16,6 +16,7 @@ class Navbar extends Component {
     visible: true,
     showMenu: false,
     showLogo: true,
+    overlay: false,
   };
 
   componentDidMount() {
@@ -63,8 +64,12 @@ class Navbar extends Component {
   };
 
   showHideMenu = () => {
-    const { showMenu, showLogo } = this.state;
-    this.setState({ showMenu: !showMenu, showLogo: !showLogo });
+    const { showMenu, showLogo, overlay } = this.state;
+    this.setState({
+      showMenu: !showMenu,
+      showLogo: !showLogo,
+      overlay: !overlay,
+    });
   };
 
   getLogoClass = showLogo => (!showLogo ? ` ${styles.hide__logo}` : '');
@@ -76,102 +81,107 @@ class Navbar extends Component {
       visible,
       showMenu,
       showLogo,
+      overlay,
     } = this.state;
     return (
-      <nav className={this.getNavbarClass(initialStatus, transparent, visible)}>
-        <div className={`${styles.container} container`}>
-          <div
-            className={`${styles['image-container']} ${this.getLogoClass(
-              showLogo,
-            )}`}
-          >
-            <Link to="/">
-              <img
-                className={styles.logo}
-                src={transparent ? logoImageLight : logoImageDark}
-                alt="logo"
-              />
-            </Link>
-          </div>
-          <div className={styles.nav__list}>
-            <button
-              type="button"
-              className={styles.menu__icon}
-              onClick={this.showHideMenu}
+      <div className={overlay ? ` ${styles.overlay}` : ''}>
+        <nav
+          className={this.getNavbarClass(initialStatus, transparent, visible)}
+        >
+          <div className={`${styles.container} container`}>
+            <div
+              className={`${styles['image-container']} ${this.getLogoClass(
+                showLogo,
+              )}`}
             >
-              <svg
-                fill={transparent ? '#ffffff' : '#000000'}
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 30 30"
-                width="30px"
-                height="30px"
+              <Link to="/">
+                <img
+                  className={styles.logo}
+                  src={transparent ? logoImageLight : logoImageDark}
+                  alt="logo"
+                />
+              </Link>
+            </div>
+            <div className={styles.nav__list}>
+              <button
+                type="button"
+                className={styles.menu__icon}
+                onClick={this.showHideMenu}
               >
-                <path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z" />
-              </svg>
-            </button>
-            {showMenu && this.getLogoClass(showLogo) ? (
-              <SlideMenu showHideMenu={this.showHideMenu} />
-            ) : (
-              <div className={styles.menu}>
-                <ul className={styles.list}>
-                  <li className={styles['list--item']}>
-                    <NavLink
-                      exact
-                      smooth
-                      className={`${
-                        styles['list--link']
-                      } ${this.getTransparentClass(transparent)}`}
-                      activeClassName={
-                        transparent
-                          ? styles['transparent--active']
-                          : styles.active
-                      }
-                      to="/#sharingBuildings"
-                      isActive={(match, { hash }) =>
-                        !!(match && match.isExact && hash)
-                      }
-                    >
-                      Report Buildings
-                    </NavLink>
-                  </li>
-                  <li className={styles['list--item']}>
-                    <NavLink
-                      exact
-                      className={`${
-                        styles['list--link']
-                      } ${this.getTransparentClass(transparent)}`}
-                      activeClassName={
-                        transparent
-                          ? styles['transparent--active']
-                          : styles.active
-                      }
-                      to="/about#about"
-                    >
-                      About
-                    </NavLink>
-                  </li>
-                  <li className={styles['list--item']}>
-                    <NavLink
-                      exact
-                      className={`${
-                        styles['list--link']
-                      } ${this.getTransparentClass(transparent)}`}
-                      activeClassName={
-                        transparent
-                          ? styles['transparent--active']
-                          : styles.active
-                      }
-                      to="/view-buildings#view"
-                    >
-                      View Buildings
-                    </NavLink>
-                  </li>
-                </ul>
-              </div>
-            )}
+                <svg
+                  fill={transparent ? '#ffffff' : '#000000'}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 30 30"
+                  width="30px"
+                  height="30px"
+                >
+                  <path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z" />
+                </svg>
+              </button>
+              {showMenu && this.getLogoClass(showLogo) ? (
+                <SlideMenu showHideMenu={this.showHideMenu} />
+              ) : (
+                <div className={styles.menu}>
+                  <ul className={styles.list}>
+                    <li className={styles['list--item']}>
+                      <NavLink
+                        exact
+                        smooth
+                        className={`${
+                          styles['list--link']
+                        } ${this.getTransparentClass(transparent)}`}
+                        activeClassName={
+                          transparent
+                            ? styles['transparent--active']
+                            : styles.active
+                        }
+                        to="/#sharingBuildings"
+                        isActive={(match, { hash }) =>
+                          !!(match && match.isExact && hash)
+                        }
+                      >
+                        Report Buildings
+                      </NavLink>
+                    </li>
+                    <li className={styles['list--item']}>
+                      <NavLink
+                        exact
+                        className={`${
+                          styles['list--link']
+                        } ${this.getTransparentClass(transparent)}`}
+                        activeClassName={
+                          transparent
+                            ? styles['transparent--active']
+                            : styles.active
+                        }
+                        to="/about#about"
+                      >
+                        About
+                      </NavLink>
+                    </li>
+                    <li className={styles['list--item']}>
+                      <NavLink
+                        exact
+                        className={`${
+                          styles['list--link']
+                        } ${this.getTransparentClass(transparent)}`}
+                        activeClassName={
+                          transparent
+                            ? styles['transparent--active']
+                            : styles.active
+                        }
+                        to="/view-buildings#view"
+                      >
+                        View Buildings
+                      </NavLink>
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     );
   }
 }
